c7ed303d3f3748623984532da3a72fde4a5b4505
katherine81779 <43974777+katherine81779@users.noreply.github.com>
2020-07-26 22:23:38 -0700

    Update getAPI.py

--- right-way/getAPI.py
+++ right-way/getAPI.py
@@ -3,7 +3,15 @@ import urllib.parse
 from urllib.request import Request, urlopen
 import requests
 
+# Majority of the time I used in this project was spent on finding an API that was 
+# free of charge and could tell us more information about the car. 
+# Initially we wanted a picture of the car to tell the user that this website knows
+# what car the user is trying to get a rating of. However, we realized that the API
+# we found for imaging was inaccurate. We decided to move on and found a query that could
+# help us at least display some information given the brand, model, and year of the car.
+
 BASE_URL = 'https://www.carqueryapi.com/api/0.3/?cmd=getTrims&' # this is to get the model id
+
 
 def getBasicURL(brand, model, year):
     query_parameters = [('make', brand), ('model', model), ('year', year)]
